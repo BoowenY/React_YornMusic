03a0206340305c96fab8ce5f21da5c8cb5329ff2
an-ger <aiboowen@gmail.com>
2021-03-31 21:04:16 +0800

    歌曲单组件

---
+++ music/src/components/SongsCover/index.js
@@ -0,0 +1,27 @@
+import React, { memo } from "react";
+import { SongsCoverWrapper } from "./style";
+import { getSizeImage, getCount } from "@/utils/format-utils";
+
+export default memo(function YNSongsCover(props) {
+  const { info, right } = props;
+  return (
+    <SongsCoverWrapper right={right}>
+      <div className="cover-top">
+        <img src={getSizeImage(info.picUrl || info.coverImgUrl, 140)} alt="" />
+        <div className="cover sprite_covor">
+          <div className="info sprite_covor">
+            <span>
+              <i className="sprite_icon erji"></i>
+              {getCount(info.playCount)}
+            </span>
+            <i className="sprite_icon play"></i>
+          </div>
+        </div>
+      </div>
+      <div className="cover-bottom text-nowrap">{info.name}</div>
+      <div className="cover-source">
+        by {info.copywriter || info.creator.nickname}
+      </div>
+    </SongsCoverWrapper>
+  );
+});
