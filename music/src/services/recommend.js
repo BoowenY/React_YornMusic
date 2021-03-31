16b29e353870ff0b940b7b29aee34d3b9161d292
an-ger <aiboowen@gmail.com>
2021-03-31 21:03:42 +0800

    推荐页面Axios配置

---
+++ music/src/services/recommend.js
@@ -0,0 +1,23 @@
+import 'axios'
+import request from './request'
+export function getTopBanners () {
+    return request({
+        url: "/banner"
+    })
+}
+export function getHotRecommends (limit) {
+    return request({
+        url: "/personalized",
+        params: {
+            limit
+        }
+    })
+}
+export function getNewAlbum (limit) {
+    return request({
+        url: "/top/album",
+        params: {
+            limit
+        }
+    })
+}
