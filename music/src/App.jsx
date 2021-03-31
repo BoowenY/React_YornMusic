a69d8041813065295514abd4d0686544afab8c48
an-ger <aiboowen@gmail.com>
2021-03-31 21:05:06 +0800

    音乐组件

--- music/src/App.jsx
+++ music/src/App.jsx
@@ -1,15 +1,21 @@
-import React, { memo} from 'react'
-import routes from './router'
-import {HashRouter} from 'react-router-dom'
-import {renderRoutes} from 'react-router-config'
-import YNHeader from './components/Header'
-import YNFooter from './components/Footer'
+import React, { memo } from "react";
+import routes from "./router";
+import { HashRouter } from "react-router-dom";
+import { renderRoutes } from "react-router-config";
+import YNHeader from "./components/Header";
+import YNFooter from "./components/Footer";
+
+import store from "./store";
+import { Provider } from "react-redux";
 export default memo(function App() {
-    return(
-        <HashRouter>
-            <YNHeader/>
-            {renderRoutes(routes)}
-            <YNFooter/>
-        </HashRouter>
-    )
-})
+  return (
+      //共享store
+    <Provider store={store}>
+      <HashRouter>
+        <YNHeader />
+        {renderRoutes(routes)}
+        <YNFooter />
+      </HashRouter>
+    </Provider>
+  );
+});
