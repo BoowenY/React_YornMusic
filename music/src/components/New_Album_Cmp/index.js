e44e09897e6a77e0dfa1386cb31f87246a62972c
an-ger <aiboowen@gmail.com>
2021-03-31 21:04:28 +0800

    新专辑组件

---
+++ music/src/components/New_Album_Cmp/index.js
@@ -0,0 +1,9 @@
+import React, { memo } from 'react'
+import Theme_Header_Recommend from '@/components/Theme_Header_Recommend'
+export default memo(function NewAlbumCmp() {
+    return (
+        <div>
+            <Theme_Header_Recommend title="新碟上架"/>
+        </div>
+    )
+})
