677992ce9d8b61c5c808dec563cfca1b5e651527
an-ger <aiboowen@gmail.com>
2021-03-31 21:04:41 +0800

    排名组件

---
+++ music/src/components/Recommend_Ranking/index.js
@@ -0,0 +1,9 @@
+import React, { memo } from 'react'
+import Theme_Header_Recommend from '@/components/Theme_Header_Recommend'
+export default memo(function Recommend_Ranking() {
+    return (
+        <div>
+            <Theme_Header_Recommend title="榜单"/>
+        </div>
+    )
+})
