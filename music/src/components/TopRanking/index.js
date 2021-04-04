095ef40ad7b5e6ba787299b1816587b022c0d7de
an-ger <aiboowen@gmail.com>
2021-04-04 20:40:54 +0800

    推荐热榜(请求有BUG)

---
+++ music/src/components/TopRanking/index.js
@@ -0,0 +1,50 @@
+import React, { memo } from "react";
+import { TopRankingWrapper } from "./style";
+import { getSizeImage } from "@/utils/format-utils";
+import { getSongDetailAction } from "@/pages/Player/store/";
+import { useDispatch } from "react-redux";
+export default memo(function YNTopRanking(props) {
+  const { info } = props;
+  const { tracks = [] } = info;
+  const dispatch = useDispatch();
+  const playMusic = (index) => {
+    dispatch(getSongDetailAction(index.id));
+  };
+  return (
+    <TopRankingWrapper>
+      <div className="header">
+        <div className="image">
+          <img src={getSizeImage(info.coverImgUrl)} alt="" />
+          <a href="/todo" className="image_cover">
+            ranking
+          </a>
+        </div>
+        <div className="info">
+          <div>
+            <button className="btn play sprite_02"></button>
+            <button className="btn favor sprite_02"></button>
+          </div>
+        </div>
+      </div>
+      <div className="list">
+        {tracks.slice(0, 10).map((item, index) => {
+          return (
+            <div key={item.id} className="list-item">
+              <div className="rank">{index + 1}</div>
+              <div className="name" onClick={(e) => playMusic(item)}>{item.name}</div>
+              <div className="operate">
+                {/* <div
+                  className="btn play sprite_02 play"
+                  onClick={(e) => playMusic(item)}
+                >???</div>
+                <div className="btn play sprite_icon2 addto"></div>
+                <div className="btn play sprite_02 favor"></div> */}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      {/* <div className="footer"></div> */}
+    </TopRankingWrapper>
+  );
+});
