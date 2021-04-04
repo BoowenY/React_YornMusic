8910428349694363915d6c932042734c24c61246
an-ger <aiboowen@gmail.com>
2021-04-04 20:42:39 +0800

    更新Redux

--- music/src/store/reducer.js
+++ music/src/store/reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux-immutable";
 import { reducer as recommend } from "../pages/Discover/c_pages/recommend/store";
+import { reducer as player } from "../pages/Player/store";
 const YNReducer = combineReducers({
-  recommend, 
+  recommend,
+  player,
 });
 export default YNReducer;
